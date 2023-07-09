83f8391de32837356afd39f9cb8407f4c53df264
Mark Watson <watsonm@netflix.com>
2023-07-09 10:44:43 -0700

    Initialise project from template repository

--- pygaposa/__meta__.py
+++ pygaposa/__meta__.py
@@ -1,12 +1,12 @@
 # `name` is the name of the package as used for `pip install package`
-name = "package-name"
+name = "pygaposa"
 # `path` is the name of the package for `import package`
 path = name.lower().replace("-", "_").replace(" ", "_")
 # Your version number should follow https://python.org/dev/peps/pep-0440 and
 # https://semver.org
 version = "0.1.dev0"
-author = "Author Name"
-author_email = ""
-description = ""  # One-liner
-url = ""  # your project homepage
-license = "Unlicense"  # See https://choosealicense.com
+author = "Mark Watson"
+author_email = "markwatson@cantab.net"
+description = "Python model for access to Gaposa morotized shades API"
+url = "https://github.com/mwatson2/pygaposa"
+license = "MIT"
